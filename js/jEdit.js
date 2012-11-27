e38bece045694a9ddc082f5a78d2135110917ee8
Alex <lilalex85@gmail.com>
2012-11-27 04:51:48 -0800

    - Optimized popups - Changed popups animation - Changed popups visualization

--- js/jEdit.js
+++ js/jEdit.js
@@ -2,7 +2,7 @@
 *
 *	Author: Alexandr Bobkov
 *	URL: http://lilalex.comuv.com/projects/jedit/demo.html
-*	Version: 1.0.1
+*	Version: 1.1.0
 *	Copyright 2012
 *
 */
@@ -50,7 +50,7 @@ var range;
 	});
 	/* Picture */
 	$('.jEdit-image-popup-close').live('click',function(){
-		closePopup('#jEdit-image-popup');
+		loadPopup('#jEdit-image-popup');
 		clearInputVal('jEdit-image-url');
 	});
 	$('#jEdit-insert-image').live('click',function(){
@@ -61,12 +61,12 @@ var range;
 		img.height = $('#jEdit-image-height').val();
 		img.appendChild(selectionContents);
 		range.insertNode(img);
-		closePopup('#jEdit-image-popup');
+		loadPopup('#jEdit-image-popup');
 		clearInputVal('jEdit-image-popup');
 	});
 	/* YouTube Video */
 	$('.jEdit-video-popup-close').live('click',function(){
-		closePopup('#jEdit-video-popup');
+		loadPopup('#jEdit-video-popup');
 		clearInputVal('jEdit-video-url');
 	});
 	$('#jEdit-insert-video').live('click',function(){
@@ -76,12 +76,12 @@ var range;
 		span.innerHTML = youTubeVideo;
 		span.appendChild(selectionContents);
 		range.insertNode(span);
-		closePopup('#jEdit-video-popup');
+		loadPopup('#jEdit-video-popup');
 		clearInputVal('jEdit-video-popup');
 	});
 	/* Hyperlink */
 	$('.jEdit-link-popup-close').live('click',function(){
-		closePopup('#jEdit-link-popup');
+		loadPopup('#jEdit-link-popup');
 		clearInputVal('jEdit-link-url');
 	});
 	$('#jEdit-insert-link').live('click',function(){
@@ -91,12 +91,12 @@ var range;
 		href.href = linkUrl;
 		href.appendChild(selectionContents);
 		range.insertNode(href);
-		closePopup('#jEdit-link-popup');
+		loadPopup('#jEdit-link-popup');
 		clearInputVal('jEdit-link-popup');
 	});
 	/* Table */
 	$('.jEdit-table-popup-close').live('click',function(){
-		closePopup('#jEdit-table-popup');
+		loadPopup('#jEdit-table-popup');
 		clearInputVal('jEdit-table-popup');
 	});
 	$('#jEdit-insert-table').live('click',function(){
@@ -116,22 +116,29 @@ var range;
 			table.appendChild(tr);
 		}
 		range.insertNode(table);
-		closePopup('#jEdit-table-popup');
+		loadPopup('#jEdit-table-popup');
 		clearInputVal('jEdit-table-popup');
 	});
   };
 })(jQuery);
 function init(elem, params){
-	var form = '<div id="jEdit-wrapper"><div id="jEdit-control-buttons"><button id="jEdit-html" title="html"></button><button id="jEdit-bold" title="bold"></button><button id="jEdit-italic" title="italic"></button><button id="jEdit-underline" title="underline"></button><button id="jEdit-list" title="list"></button><button id="jEdit-img" title="picture"></button><button id="jEdit-youtube" title="video"></button><button id="jEdit-link" title="link"></button><button id="jEdit-unlink" title="unlink"></button><button id="jEdit-table" title="table"></button></div><div id="jEdit" contenteditable="true">'+params.txt+'</div><textarea id="jEdit-html-view"></textarea></div>';
+	var formID = '#jEdit-wrapper';
+	var form = '<div id="jEdit-wrapper"></div>';
+	var controlButtons = '<div id="jEdit-control-buttons"><button id="jEdit-html" title="html"></button><button id="jEdit-bold" title="bold"></button><button id="jEdit-italic" title="italic"></button><button id="jEdit-underline" title="underline"></button><button id="jEdit-list" title="list"></button><button id="jEdit-img" title="picture"></button><button id="jEdit-youtube" title="video"></button><button id="jEdit-link" title="link"></button><button id="jEdit-unlink" title="unlink"></button><button id="jEdit-table" title="table"></button></div>';
+	var formTextArea = '<div id="jEdit" contenteditable="true">'+params.txt+'</div><textarea id="jEdit-html-view"></textarea>';
+	var imagePopup = '<div class="popup-wrapper jEdit-popup" id="jEdit-image-popup"><div class="popup-content-wrapper"><h4>Copy/Past image url.</h4><ul class="popup-content"><li><label>Width:</label><input type="text" id="jEdit-image-width"/></li><li><label>Height:</label><input type="text" id="jEdit-image-height"/></li><li><label>Url:</label><input type="text" id="jEdit-image-url"/></li></ul></div><hr/><div class="popup-footer-wrapper"><div><input type="button" id="jEdit-insert-image" value="Insert"/><div class="styledButtonOverlay"></div></div><div><input type="button" class="jEdit-image-popup-close" value="Cancel"/><div class="styledButtonOverlay"></div></div></div></div>';
+	var videoPopup = '<div class="popup-wrapper jEdit-popup" id="jEdit-video-popup"><div class="popup-content-wrapper"><h4>Copy/Past youtube video url.</h4><ul class="popup-content"><li><label>Width:</label><input type="text" id="jEdit-video-width"/></li><li><label>Height:</label><input type="text" id="jEdit-video-height"/></li><li><label>Url:</label><input type="text" id="jEdit-video-url"/></li></ul></div><hr/><div class="popup-footer-wrapper"><div><input type="button" id="jEdit-insert-video" value="Insert"/><div class="styledButtonOverlay"></div></div><div><input type="button" class="jEdit-video-popup-close" value="Cancel"/><div class="styledButtonOverlay"></div></div></div></div>';
+	var linkPopup = '<div class="popup-wrapper jEdit-popup" id="jEdit-link-popup"><div class="popup-content-wrapper"><h4>Copy/Past link url.</h4><ul class="popup-content"><li><label>Url:</label><input type="text" id="jEdit-link-url"/></li></ul></div><hr/><div class="popup-footer-wrapper"><div><input type="button" id="jEdit-insert-link" value="Insert"/><div class="styledButtonOverlay"></div></div><div><input type="button" class="jEdit-link-popup-close" value="Cancel"/><div class="styledButtonOverlay"></div></div></div></div>';
+	var tablePopup = '<div class="popup-wrapper jEdit-popup" id="jEdit-table-popup"><div class="popup-content-wrapper"><h4>Specify rows and colums for the table.</h4><ul class="popup-content"><li><label>Rows:</label><input type="text" id="jEdit-table-rows"/></li><li><label>Colums:</label><input type="text" id="jEdit-table-cols"/></li></ul></div><hr/><div class="popup-footer-wrapper"><div><input type="button" id="jEdit-insert-table" value="Insert"/><div class="styledButtonOverlay"></div></div><div><input type="button" class="jEdit-table-popup-close" value="Cancel"/><div class="styledButtonOverlay"></div></div></div></div>';
+	
 	$(elem).append(form);
-	var imagePopup = '<div class="popup-wrapper jEdit-popup" id="jEdit-image-popup"><div class="popup-header-wrapper"><h6>Insert Image</h6><div class="popup-close-button jEdit-image-popup-close"></div></div><div class="popup-content-wrapper"><h4>Copy/Past image url.</h4><ul class="popup-content"><li><label>Width:</label><input type="text" id="jEdit-image-width"/></li><li><label>Height:</label><input type="text" id="jEdit-image-height"/></li><li><label>Url:</label><input type="text" id="jEdit-image-url"/></li></ul></div><hr/><div class="popup-footer-wrapper"><div><input type="button" id="jEdit-insert-image" value="Insert"/><div class="styledButtonOverlay"></div></div><div><input type="button" class="jEdit-image-popup-close" value="Cancel"/><div class="styledButtonOverlay"></div></div></div></div>';
-	$('body').append(imagePopup);
-	var videoPopup = '<div class="popup-wrapper jEdit-popup" id="jEdit-video-popup"><div class="popup-header-wrapper"><h6>Insert YouTube Video</h6><div class="popup-close-button jEdit-video-popup-close"></div></div><div class="popup-content-wrapper"><h4>Copy/Past youtube video url.</h4><ul class="popup-content"><li><label>Width:</label><input type="text" id="jEdit-video-width"/></li><li><label>Height:</label><input type="text" id="jEdit-video-height"/></li><li><label>Url:</label><input type="text" id="jEdit-video-url"/></li></ul></div><hr/><div class="popup-footer-wrapper"><div><input type="button" id="jEdit-insert-video" value="Insert"/><div class="styledButtonOverlay"></div></div><div><input type="button" class="jEdit-video-popup-close" value="Cancel"/><div class="styledButtonOverlay"></div></div></div></div>';
-	$('body').append(videoPopup);
-	var linkPopup = '<div class="popup-wrapper jEdit-popup" id="jEdit-link-popup"><div class="popup-header-wrapper"><h6>Insert Hyperlink</h6><div class="popup-close-button jEdit-link-popup-close"></div></div><div class="popup-content-wrapper"><h4>Copy/Past link url.</h4><ul class="popup-content"><li><label>Url:</label><input type="text" id="jEdit-link-url"/></li></ul></div><hr/><div class="popup-footer-wrapper"><div><input type="button" id="jEdit-insert-link" value="Insert"/><div class="styledButtonOverlay"></div></div><div><input type="button" class="jEdit-link-popup-close" value="Cancel"/><div class="styledButtonOverlay"></div></div></div></div>';
-	$('body').append(linkPopup);
-	var tablePopup = '<div class="popup-wrapper jEdit-popup" id="jEdit-table-popup"><div class="popup-header-wrapper"><h6>Insert Table</h6><div class="popup-close-button jEdit-table-popup-close"></div></div><div class="popup-content-wrapper"><h4>Specify rows and colums for the table.</h4><ul class="popup-content"><li><label>Rows:</label><input type="text" id="jEdit-table-rows"/></li><li><label>Colums:</label><input type="text" id="jEdit-table-cols"/></li></ul></div><hr/><div class="popup-footer-wrapper"><div><input type="button" id="jEdit-insert-table" value="Insert"/><div class="styledButtonOverlay"></div></div><div><input type="button" class="jEdit-table-popup-close" value="Cancel"/><div class="styledButtonOverlay"></div></div></div></div>';
-	$('body').append(tablePopup);
+	$(formID).append(controlButtons);
+	$(formID).append(imagePopup);
+	$(formID).append(videoPopup);
+	$(formID).append(linkPopup);
+	$(formID).append(tablePopup);
+	$(formID).append(formTextArea);
+	
 	if(params.width > 0){
 		$('#jEdit-wrapper').css('width', params.width+'px')
 	}
@@ -160,16 +167,12 @@ function getVideoHtml(videoUrl, width, height){
 	var youTubeVideo = '<object width="425" height="344"><param name="movie" value="http://youtube.googleapis.com/v/'+videoID+'&hl=en&fs=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://youtube.googleapis.com/v/'+videoID+'&hl=en&fs=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="'+width+'" height="'+height+'"></embed></object>';
 	return youTubeVideo;
 }
-function loadPopup(popUpId){  
-	$(popUpId).center(true);
-	$(popUpId).fadeIn('fast', function() {});
+function loadPopup(popUpID){
+	$('.jEdit-popup').each(function(){
+		var currentID = '#' + $(this).attr('id');
+		if(currentID != popUpID){
+			$(this).slideUp('fast');
+		}
+	});
+	$(popUpID).slideToggle('fast');
 }
-function closePopup(popUpId){  
-	$(popUpId).fadeOut('fast', function() {});
-}
-jQuery.fn.center = function () {
-    this.css("position","absolute");
-    this.css("top", (($(window).height() - this.outerHeight()) / 2) + $(window).scrollTop() + "px");
-    this.css("left", (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft() + "px");
-    return this;
-};
